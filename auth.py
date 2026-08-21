14d6bde0ef7a909a111b06474d2fc3bfc1a500cc
Benjamin Tutu <tutu.benjamin65@gmail.com>
2026-08-21 19:55:51 +0000

    Added authentication and jwt

--- auth.py
+++ auth.py
@@ -1,14 +1,14 @@
 import os
-from datetime import timedelta, datetime, timezone
+from datetime import timedelta, datetime, timezone, date
 from typing import Annotated
 from passlib.context import CryptContext
 
 from fastapi import APIRouter, HTTPException, Depends, status, Request
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from passlib.handlers.des_crypt import bigcrypt
 from pydantic import BaseModel, Field, ConfigDict
 from sqlalchemy.orm import Session
+from jose import jwt, JWTError
 
 from database import SessionLocal
 from models import User
@@ -22,6 +22,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer("/auth/token")
+
 
 def get_db():
     db = SessionLocal()
@@ -32,11 +34,11 @@ def get_db():
 
 class UserCreate(BaseModel):
     username: str = Field(min_length=3)
-    email: str = Field(min_length=3)
-    password: str = Field(min_length=8)
+    email: str = Field(min_length=3, max_length=45)
+    password: str = Field(min_length=3, max_length=72)
     is_active: bool = Field(default=True)
     role: str = Field(default="user")
-    date_joined: datetime = Field(default=datetime.now(timezone.utc))
+    date_joined: datetime = Field(default_factory=date.today)
 
     model_config = ConfigDict(
         from_attributes=True,
@@ -52,7 +54,46 @@ class UserCreate(BaseModel):
         }
     )
 
-@router.post("/register")
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    is_active: bool
+    role: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+def authenticate_user(username: str, password: str, db: Session):
+    user_model = db.query(User).filter(User.username == username).first()
+    if not user_model:
+        return False
+    if not bcrypt_context.verify(password, user_model.password):
+        return False
+    return user_model
+
+def create_access_token(username:str, user_id: int, expires_delta: timedelta):
+    payload = {
+        "sub": username,
+        "id": user_id,
+        "exp": datetime.now(timezone.utc) + expires_delta,
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(status_code=404, detail="Could not validate credentials")
+        return {'id': user_id, 'username': username}
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
+
+@router.post("/register", response_model=UserCreate, status_code=201)
 async def register(user: UserCreate, db:Session = Depends(get_db)):
     user = User(
         username=user.username,
@@ -66,6 +107,18 @@ async def register(user: UserCreate, db:Session = Depends(get_db)):
     db.commit()
     db.refresh(user)
     return user
+
+
+@router.post("/token", response_model=Token, status_code=200)
+async def create_access_token_login(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        db:Session = Depends(get_db)
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user or not bcrypt_context.verify(form_data.password, user.password):
+        raise HTTPException(status_code=400, detail="Incorrect password")
+    token = create_access_token(user.username, user.id, timedelta(minutes=30))
+    return {"access_token": token, "token_type": "bearer"}
 
 
 
--- blog.py
+++ blog.py
@@ -2,11 +2,14 @@ from datetime import date
 
 from fastapi import Depends, APIRouter, HTTPException, status
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.persistence import post_update
+from sqlalchemy.sql.functions import current_user
 
 from database import SessionLocal
 from typing import Annotated
 from pydantic import BaseModel, Field, ConfigDict
 from models import *
+from auth import get_current_user
 
 
 router = APIRouter(
@@ -28,14 +31,26 @@ class AuthorResponse(BaseModel):
     username: str
     id: int
 
+class PostCreate(BaseModel):
+    title: str
+    body: str
+    slug: str
+    date_posted: date = Field(default_factory=date.today)
+
 class PostResponse(BaseModel):
     title: str = Field(min_length=3)
     body: str = Field(min_length=3, max_length=200)
     slug: str = Field(min_length=1)
-    date_posted: date = Field(default=datetime.date.today)
+    date_posted: date = Field(default_factory=date.today)
     author: AuthorResponse
 
     model_config = ConfigDict(from_attributes=True)
+
+class PostUpdate(BaseModel):
+    title: str
+    body: str
+    slug: str
+    date_posted: date = Field(default_factory=date.today)
 
 @router.get("/posts")
 async def get_posts(db:db_dependency):
@@ -44,19 +59,41 @@ async def get_posts(db:db_dependency):
 
 
 @router.post("/create-post", response_model=PostResponse)
-async def create_post(db:db_dependency, post: PostResponse):
+async def create_post(db:db_dependency, post: PostCreate, user: Annotated[dict, Depends(get_current_user)]):
+
     new_post = Post(
         title=post.title,
         body=post.body,
         slug=post.slug,
-        date_posted=post.date_posted
+        date_posted=post.date_posted,
+        author_id=user['id']
     )
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
     return new_post
 
+@router.get("/post")
+async def get_post(db:db_dependency, user: Annotated[dict, Depends(get_current_user)]):
+    post = db.query(Post).filter(Post.author_id == user['id'] ).first()
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
 
+@router.put("/update-post/{post_id}", response_model=PostResponse)
+async def update_post(db: db_dependency,
+                      post: PostUpdate,
+                      post_id: int,
+                      user: Annotated[dict, Depends(get_current_user)]
+                      ):
+    post_model = db.query(Post).filter((Post.id == post_id) & (Post.author_id == user['id']) ).first()
+    if not post_model:
+        raise HTTPException(status_code=404, detail="Post not found")
+    post_model.title = post.title
+    post_model.body = post.body
+    post_model.slug = post.slug
+    post_model.date_posted = post.date_posted
 
-
-
+    db.add(post_model)
+    db.commit()
+    return post_model
